69a39afcc53a52ca13dd6bea80e3bc007a2c2ed9
FraCipolla <matcip@hotmail.com>
2023-01-25 16:02:06 +0100

    server start / server listen

--- includes/Server.class.hpp
+++ includes/Server.class.hpp
@@ -3,6 +3,11 @@
 
 # include <Parser.class.hpp>
 # include <webserv.hpp>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netdb.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
 
 class Server
 {
@@ -14,6 +19,12 @@ class Server
 		std::vector<t_config>	_config;
 		const char*				_filename;
 		std::vector<VirtServ*>	_virtServs;
+		int						_sockfd;
+		struct addrinfo			*_addrStruct;
+
+		bool	startServer();
+		void	closeServer();
+		bool	listenServer();
 
 	public:
 		Server(const char* filename);
--- srcs/Server.class.cpp
+++ srcs/Server.class.cpp
@@ -17,8 +17,41 @@ Server::Server(const char* filename) : _filename(filename == NULL ? "default.con
 		iterConfig++;
 	}
 	std::cout << "ciao" << std::endl;
+
+	// QUA BISOGNA RIEMPIRE LA STRUCT _addStruct
+
+		_addrStruct->ai_family = AF_INET;  // Address family, AF_INET
+    	_addrStruct->ai_socktype = 0; 
+		_addrStruct->ai_protocol = 0;   // Port number
+		_addrStruct->ai_family = AI_PASSIVE;
+    	_addrStruct->ai_addr->sa_family = AF_INET;
+		//_addrStruct->ai_addr->sa_data = inet_addr("IP ADDRESS HERE")
 }
 
 Server::~Server() {}
 
-	
+bool	Server::startServer()
+{
+	_sockfd = socket(_addrStruct->ai_family, _addrStruct->ai_socktype, _addrStruct->ai_protocol);
+	if (_sockfd < 0)
+	{
+		std::cout << "socket error" << std::endl;
+		return false;
+	}
+	if((bind(_sockfd, _addrStruct->ai_addr, _addrStruct->ai_addrlen)) < 0)
+	{
+		std::cout << "bind error" << std::endl;
+		return false;
+	}
+	return true;
+}	
+
+bool	Server::listenServer()
+{
+	if (listen(_sockfd, 20) < 0)
+	{
+		std::cout << "listen failed" << std::endl;
+		return false;
+	}
+	return true;
+}
